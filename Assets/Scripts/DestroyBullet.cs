9c92c89cb5aaa22239b0ecea44b66226c7b1a596
302-Brown-Dennis <Dennisbrown89701@yahoo.com>
2023-04-16 00:30:05 -0700

    fix code merge issues

--- Assets/Scripts/DestroyBullet.cs
+++ Assets/Scripts/DestroyBullet.cs
@@ -2,28 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< Updated upstream:Assets/Scripts/EnemyScripts/EnemyController.cs
-public class EnemyController : MonoBehaviour
-{
-    // Enemy HP
-    int health;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        health = 3;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(health == 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-========
 public class DestroyBullet : MonoBehaviour
 {
     void Start()
@@ -31,10 +10,8 @@ public class DestroyBullet : MonoBehaviour
         Destroy(gameObject, 2f);
     }
 
->>>>>>>> Stashed changes:Assets/Scripts/DestroyBullet.cs
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Bullet")    
-            health -= 1;
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -2,39 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< Updated upstream:Assets/Scripts/EnemyScripts/EnemyController.cs
 public class EnemyController : MonoBehaviour
 {
     // Enemy HP
-    int health;
-    
+    private int health;
+    private int mhealth;
     // Start is called before the first frame update
     void Start()
     {
-        health = 3;
+        mhealth = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health == 0)
+        if(mhealth == 0)
         {
             Destroy(gameObject);
         }
     }
 
-========
-public class DestroyBullet : MonoBehaviour
-{
-    void Start()
-    {
-        Destroy(gameObject, 2f);
-    }
-
->>>>>>>> Stashed changes:Assets/Scripts/DestroyBullet.cs
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Bullet")    
-            health -= 1;
+        if(collision.gameObject.tag == "Bullet")
+            {
+                mhealth--;
+            }
+                
     }
 }
