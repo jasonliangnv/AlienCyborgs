3f4aa81d903f40ac5ca73d3c1fa978db1bc664ad
jasonliangnv <jasonliang2121@gmail.com>
2023-05-07 08:04:43 -0700

    Updated Boss AI
    Added phases to the boss fight, as well as added new attack patterns and movement

--- Assets/Scripts/BossCombat.cs
+++ Assets/Scripts/BossCombat.cs
@@ -21,10 +21,15 @@ public class BossCombat : MonoBehaviour
     private float timeSinceLastFire = 0f;
 
     // Checks how many normal attacks the boss has made
-    private int attackTracker = 0;
+    private int attackTracker1 = 0;
+    private int attackTracker2 = 0;
+    private int attackTracker3 = 0;
+    private int totalHealth = 0;
 
     private void Start()
     {
+        // Grabs the enemy total HP
+        totalHealth = GetComponent<EnemyHealth>().health;
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
@@ -38,19 +43,51 @@ public class BossCombat : MonoBehaviour
             timeSinceLastFire = 0f;
 
             // Different fire rate changes for bosses vs regular enemies
-            if (attackTracker < 10)
+            if (attackTracker1 < 10)
             {
-                fireRate = 5f;
-                attackTracker++;
+                fireRate = 2f;
+                attackTracker1++;
+                attackTracker2++;
+                attackTracker3++;
             }
-            // Fires a special fast bullet every 10 attacks
-            else if (attackTracker == 10)
+            // Fires a special fast bullet every 10 attacks and when HP is below 90%
+            else if (attackTracker1 >= 10 && ((float)GetComponent<EnemyHealth>().health/totalHealth)*100 <= 90)
             {
                 bulletSpeed = 10f;
                 FireTripleBullet();
 
-                bulletSpeed = 1f;
-                attackTracker = 0;
+                bulletSpeed = 0.5f;
+                attackTracker1 = 0;
+            }
+            
+            // Fires special attacks at 75% HP or below
+            if(attackTracker2 >= 5 && ((float)GetComponent<EnemyHealth>().health/totalHealth)*100 <= 75)
+            {
+                bulletSpeed = 5f;
+
+                FireRandomBullet();
+                
+                bulletSpeed = 0.5f;
+                attackTracker2 = 0;
+            }
+
+            
+            // Fires special attacks at 50% HP or below
+            if(attackTracker3 >= 5 && ((float)GetComponent<EnemyHealth>().health/totalHealth)*100 <= 50)
+            {
+                GetComponent<EnemyAiController>().speed = 1;
+                bulletSpeed = 0f;
+
+                BulletMine();
+
+                bulletSpeed = 0.5f;
+                attackTracker3 = 0;
+            }
+
+            // Cranks the speed up near death
+            if(((float)GetComponent<EnemyHealth>().health/totalHealth)*100 <= 10)
+            {
+                GetComponent<EnemyAiController>().speed = 5;
             }
         }
     }
@@ -60,6 +97,58 @@ public class BossCombat : MonoBehaviour
         direction.Normalize();
         GameObject bullet = Instantiate(bulletPrefab, EnemyFirePoint.position, EnemyFirePoint.rotation);
         bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+    }
+
+    void FireRandomBullet()
+    {
+        Vector3 direction = (playerTransform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f))) - transform.position;
+        direction.Normalize();
+        GameObject bullet = Instantiate(bulletPrefab, EnemyFirePoint.position, EnemyFirePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;        
+    }
+
+    // Fires a diamond bullet mine based on the enemies location
+    void BulletMine()
+    {
+        Vector3 direction = transform.position + new Vector3(0, 1, 0);
+        direction.Normalize();
+        GameObject bullet1 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(0, 1, 0), EnemyFirePoint.rotation);
+        bullet1.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        direction = transform.position + new Vector3(0.5f, 0.5f, 0);
+        direction.Normalize();
+        GameObject bullet2 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(0.5f, 0.5f, 0), EnemyFirePoint.rotation);
+        bullet2.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        direction = transform.position + new Vector3(1, 0, 0);
+        direction.Normalize();
+        GameObject bullet3 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(1, 0, 0), EnemyFirePoint.rotation);
+        bullet3.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        direction = transform.position + new Vector3(1, -0.5f, 0);
+        direction.Normalize();
+        GameObject bullet4 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(0.5f, -0.5f, 0), EnemyFirePoint.rotation);
+        bullet4.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        direction = transform.position + new Vector3(0, -1, 0);
+        direction.Normalize();
+        GameObject bullet5 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(0, -1, 0), EnemyFirePoint.rotation);
+        bullet5.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        direction = transform.position + new Vector3(-0.5f, -0.5f, 0);
+        direction.Normalize();
+        GameObject bullet6 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(-0.5f, -0.5f, 0), EnemyFirePoint.rotation);
+        bullet6.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        direction = transform.position + new Vector3(-1, 0, 0);
+        direction.Normalize();
+        GameObject bullet7 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(-1, 0, 0), EnemyFirePoint.rotation);
+        bullet7.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        direction = transform.position + new Vector3(0.5f, -0.5f, 0);
+        direction.Normalize();
+        GameObject bullet8 = Instantiate(bulletPrefab, EnemyFirePoint.position + new Vector3(-0.5f, 0.5f, 0), EnemyFirePoint.rotation);
+        bullet8.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
 
     void FireTripleBullet()
